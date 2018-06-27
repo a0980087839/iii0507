ea2fe8b0e06b7330657fc593077b13226d8ff895
ninolin <a7872278722@gmail.com>
2018-06-27 21:14:15 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -18,12 +18,7 @@
 	
 	$response = array (
 		"replyToken" => $sender_replyToken,
-		"messages" => array (
-			array (
-				"type" => "text",
-				"text" => "Hello. You say". $course_name
-			)
-		)
+		"messages" => welcome()
 	);
 	
 	fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
@@ -37,12 +32,39 @@
 	$result = curl_exec($ch);
 	curl_close($ch);
   
+	function welcome(){
+		$json = '{
+		  "type": "template",
+		  "altText": "this is a buttons template",
+		  "template": {
+			"type": "buttons",
+			"actions": [
+			  {
+				"type": "postback",
+				"label": "報名課程",
+				"text": "apply",
+				"data": "aaa"
+			  },
+			  {
+				"type": "postback",
+				"label": "我的課程",
+				"text": "my",
+				"data": "ccc"
+			  }
+			],
+			"title": "歡迎來到龍鳳行銷",
+			"text": "您可以報名新課程或查看已報名的課程"
+		  }
+		}';
+		return json_decode($json_str);
+	}
+	
 	function sql_select_fetchALL($sql)
 	{   
 		$db_server = "localhost";
-		$db_name = "course_management_t";
-		$db_user = "root";
-		$db_passwd = "fdd396906f5054060122311cf8b0eb2da0cfe7a437501152";
+		$db_name = "dpcleader_course";
+		$db_user = "dpcleader_sup";
+		$db_passwd = "Ab09800878390";
 		
 		$con=mysqli_connect($db_server, $db_user, $db_passwd) or die("資料庫登入錯誤");
 		if(mysqli_connect_errno($con)){
